812ae9850a77d269d5c12a8d168b0c68f9956be7
Yamizaki Arakaki <124403912+Yamizaki@users.noreply.github.com>
2024-04-24 15:37:10 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from routes.todo import todo
+
+from dotenv import load_dotenv
+import os
+from config.mongodb import mongo
+
+
+def init_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+
+    # Conexion con Mongo
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    mongo.init_app(app)
+    # Endpoints
+
+
+    @app.route("/")
+    def index():
+        return render_template("index.html")
+
+
+    ##############
+
+
+    ##############
+
+
+    # Trababa de manera similar al URL PATTERNS ed DJANGO
+    app.register_blueprint(todo, url_prefix="/todo")
+
+    return app
+# Inicializador
+if __name__ == "__main__":
+    app = init_app()
+    app.run()
