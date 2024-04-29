25d550a08e34d5246a038b25ebe1be5a40019bf3
Yamizaki <Arakaki_18@outlook.com>
2024-04-28 19:55:32 -0500

    Modificando ENDPOINT

--- routes/todo.py
+++ routes/todo.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from services.todo import createTodoService, getTodoService, getOneTodoService, updateTodoService, deleteTodoService
+from services.todo import createTodoService, getTodoService, getOneTodoService, updateTodoService, deleteTodoService, createCertificadoService, getCertificadoService
 todo = Blueprint('todo', __name__)
 
 @todo.route('/', methods=['GET'])
@@ -23,3 +23,13 @@ def updateTodo(id):
 @todo.route('/<id>', methods=['DELETE'])
 def deleteTodo(id):
     return deleteTodoService(id)
+
+
+#CERTIFICADO 
+@todo.route('/createCertificado', methods=['POST'])
+def createCertificado():
+    return createCertificadoService()
+
+@todo.route('/getCertificado', methods=['GET'])
+def getCertificado():
+    return getCertificadoService()
--- services/todo.py
+++ services/todo.py
@@ -3,67 +3,106 @@ from flask import request, Response
 
 
 def createTodoService():
-    
-    
-    title = request.form['title']  
-    description = request.form['description']
-    file = request.files['file']
-    technologies = request.form['technologies']
-    proyect_link = request.form['proyect_link']
-    code_source = request.form['code_source']
+
+    title = request.form["title"]
+    description = request.form["description"]
+    file = request.files["file"]
+    technologies = request.form["technologies"]
+    proyect_link = request.form["proyect_link"]
+    code_source = request.form["code_source"]
 
     try:
         image_data = file.read()
-        image_id = mongo.db.todos.insert_one({  'title': title,
-                                                'image': image_data,
-                                                'description': description,
-                                                'technologies': technologies,
-                                                'proyect_link': proyect_link,
-                                                'code_source': code_source
-                                                }).inserted_id
-       
-        return 'Se agregó correctamente'
-    
-    except():
-        return 'Hubo un error al agregar un proyecto'
+        image_id = mongo.db.proyectos.insert_one(
+            {
+                "title": title,
+                "image": image_data,
+                "description": description,
+                "technologies": technologies,
+                "proyect_link": proyect_link,
+                "code_source": code_source,
+            }
+        ).inserted_id
+
+        return "Se agregó correctamente"
+
+    except ():
+        return "Hubo un error al agregar un proyecto"
 
 
 def getTodoService():
     from bson import json_util
-    
-    data = mongo.db.todos.find()
+
+    data = mongo.db.proyectos.find()
     result = json_util.dumps(data)
-    return Response(result, mimetype='application/json')
+    return Response(result, mimetype="application/json")
 
 
 def getOneTodoService(id):
     from bson import json_util, ObjectId
-    
-    data = mongo.db.todos.find_one({'_id': ObjectId(id)})
+
+    data = mongo.db.proyectos.find_one({"_id": ObjectId(id)})
     result = json_util.dumps(data)
-    return Response(result, mimetype='application/json')
+    return Response(result, mimetype="application/json")
+
 
 def updateTodoService(id):
-    from bson import  ObjectId
-    
+    from bson import ObjectId
+
     data = request.get_json()
     if len(data) == 0:
-        return 'Invalid Update', 400
-    
-    result = mongo.db.todos.find_one({'_id': ObjectId(id)}, {'$set' : data})
-    
+        return "Invalid Update", 400
+
+    result = mongo.db.proyectos.find_one({"_id": ObjectId(id)}, {"$set": data})
+
     if result.modified_count > 1:
-        return 'Succefully update', 200
+        return "Succefully update", 200
     else:
-        return 'Something went wrong', 404
-   
+        return "Something went wrong", 404
+
 
 def deleteTodoService(id):
     from bson import json_util, ObjectId
-    
-    data = mongo.db.todos.delete_one({'_id': ObjectId(id)})
-  
-    return 'Todo selected was delete'
+
+    data = mongo.db.proyectos.delete_one({"_id": ObjectId(id)})
+
+    return "Todo selected was delete"
 
 
- 
+
+
+### CERTIFICADO 
+def createCertificadoService():
+
+    title = request.form["title"]
+    description = request.form["description"]
+    file = request.files["file"]
+    technologies = request.form["technologies"]
+    proyect_link = request.form["certificado_link"]
+   
+
+    try:
+        image_data = file.read()
+        image_id = mongo.db.certificado.insert_one(
+            {
+                "title": title,
+                "image": image_data,
+                "description": description,
+                "technologies": technologies,
+                "proyect_link": proyect_link,
+                
+            }
+        ).inserted_id
+
+        return "Se agregó correctamente"
+
+    except ():
+        return "Hubo un error al agregar un proyecto"
+
+
+def getCertificadoService():
+    from bson import json_util
+
+    data = mongo.db.certificado.find()
+    result = json_util.dumps(data)
+    return Response(result, mimetype="application/json")
